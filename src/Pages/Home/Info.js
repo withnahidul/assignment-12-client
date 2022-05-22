e57ea0451a72e02ff5c49b0ffa93d639b9466d21
withnahidul <nahidulislam6800@gmail.com>
2022-05-22 21:24:18 +0600

    info-edited

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -7,9 +7,9 @@ import marker from '../../assets/icons/marker.svg';
 const Info = () => {
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 '>
-            <InfoCard cardTitle="Openning Hours" cardInfo="9am to 10pm" bgclassName="bg-gradient-to-r from-secondary to-primary" img={clock}></InfoCard>
-            <InfoCard cardTitle="Contract Us" cardInfo="11589" bgclassName="bg-neutral" img={phone}></InfoCard>
-            <InfoCard cardTitle="Our Address" cardInfo="Dhaka-0101" bgclassName="bg-gradient-to-r from-secondary to-primary" img={marker}></InfoCard>
+            <InfoCard cardTitle="Openning Hours" cardInfo="9am to 10pm" bgClass="bg-gradient-to-r from-secondary to-primary" img={clock}></InfoCard>
+            <InfoCard cardTitle="Contract Us" cardInfo="11589" bgClass="bg-neutral" img={phone}></InfoCard>
+            <InfoCard cardTitle="Our Address" cardInfo="Dhaka-0101" bgClass="bg-gradient-to-r from-secondary to-primary" img={marker}></InfoCard>
         </div>
     );
 };
