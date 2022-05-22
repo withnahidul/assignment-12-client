d8f5078912fe5b39de1596a4667c53fd192b74a5
withnahidul <nahidulislam6800@gmail.com>
2022-05-22 21:20:38 +0600

    update-footer

--- src/Pages/Home/Footer.js
+++ src/Pages/Home/Footer.js
@@ -1,29 +1,40 @@
 import React from 'react';
+import footer from '../../assets/images/footer.png';
 
 const Footer = () => {
     return (
-        <footer class="footer p-10 bg-neutral text-neutral-content">
-            <div>
-                <span class="footer-title">Services</span>
-                <a class="link link-hover">Branding</a>
-                <a class="link link-hover">Design</a>
-                <a class="link link-hover">Marketing</a>
-                <a class="link link-hover">Advertisement</a>
+        <footer style={{
+            background: `url(${footer})`,
+            bacgroundSize: 'cover'
+        }} className=" p-10 ">
+            <div className='footer'>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </div>
             </div>
-            <div>
-                <span class="footer-title">Company</span>
-                <a class="link link-hover">About us</a>
-                <a class="link link-hover">Contact</a>
-                <a class="link link-hover">Jobs</a>
-                <a class="link link-hover">Press kit</a>
+            <div className='my-10 text-center'>
+                <p>Copyright © 2022 - All right reserved by Nahidul Islam</p>
             </div>
-            <div>
-                <span class="footer-title">Legal</span>
-                <a class="link link-hover">Terms of use</a>
-                <a class="link link-hover">Privacy policy</a>
-                <a class="link link-hover">Cookie policy</a>
-            </div>
-        </footer>
+
+
+        </footer >
     );
 };
 
